2574d757aa15c0013db9270c388bf942c395c074
riskymind <thisiskelchi@gmail.com>
2020-05-17 13:29:43 +0100

    made adjustment to the toolbar

--- app/src/main/java/com/example/nescafe_pushcart/ui/VendorAuthFragment.kt
+++ app/src/main/java/com/example/nescafe_pushcart/ui/VendorAuthFragment.kt
@@ -1,10 +1,11 @@
-package com.example.nescafe_pushcart
+package com.example.nescafe_pushcart.ui
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import com.example.nescafe_pushcart.R
 
 /**
  * A simple [Fragment] subclass.
