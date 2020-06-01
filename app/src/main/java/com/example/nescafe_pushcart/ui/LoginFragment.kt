4513f05f904bd8e17526da9ea1b72477e172b675
Osehi <osehiaseehilen@gmail.com>
2020-06-02 00:51:14 +0100

    new nav direction

--- app/src/main/java/com/example/nescafe_pushcart/ui/LoginFragment.kt
+++ app/src/main/java/com/example/nescafe_pushcart/ui/LoginFragment.kt
@@ -5,7 +5,9 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 import com.example.nescafe_pushcart.R
+import kotlinx.android.synthetic.main.fragment_login.*
 
 /**
  * A simple [Fragment] subclass.
@@ -18,6 +20,19 @@ class LoginFragment : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_login, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        login_btn.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_nescafeKitchen)
+        }
+
+        reset_password.setOnClickListener {
+            findNavController().navigate(R.id.action_loginFragment_to_vendorProfile)
+        }
+
     }
 
 }
--- app/src/main/java/com/example/nescafe_pushcart/ui/VendorProfile.kt
+++ app/src/main/java/com/example/nescafe_pushcart/ui/VendorProfile.kt
@@ -5,8 +5,10 @@ import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
 
 import com.example.nescafe_pushcart.R
+import kotlinx.android.synthetic.main.fragment_vendor_profile.*
 
 /**
  * A simple [Fragment] subclass.
@@ -19,6 +21,15 @@ class VendorProfile : Fragment() {
     ): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_vendor_profile, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+
+        thisFab.setOnClickListener {
+            findNavController().navigate(R.id.action_vendorProfile_to_vendorAuthFragment)
+        }
+
     }
 
 }
