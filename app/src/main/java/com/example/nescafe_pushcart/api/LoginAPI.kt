03789640629db52e51d98822573095001a1edac8
Osehi <osehiaseehilen@gmail.com>
2020-06-04 18:32:53 +0100

    loginAPI and retrofit instance created

--- app/src/main/java/com/example/nescafe_pushcart/api/LoginAPI.kt
+++ app/src/main/java/com/example/nescafe_pushcart/api/LoginAPI.kt
@@ -1,8 +1,12 @@
 package com.example.nescafe_pushcart.api
 
+import com.example.nescafe_pushcart.constants.URLConstants
 import com.example.nescafe_pushcart.model.login.LoginBody
 import com.example.nescafe_pushcart.model.login.SignInResponse
+import okhttp3.OkHttpClient
 import retrofit2.Response
+import retrofit2.Retrofit
+import retrofit2.converter.gson.GsonConverterFactory
 import retrofit2.http.Body
 
 interface LoginAPI {
@@ -15,6 +19,15 @@ interface LoginAPI {
     companion object {
 
         operator fun invoke():LoginAPI {
+
+            val apiClient = OkHttpClient.Builder().build()
+
+            return Retrofit.Builder()
+                .addConverterFactory(GsonConverterFactory.create())
+                .baseUrl(URLConstants.BASE_URL)
+                .client(apiClient)
+                .build()
+                .create(LoginAPI::class.java)
 
         }
 
