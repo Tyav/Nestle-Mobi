025ff4df2be5a55680e89a9da6f117903b79a476
Osehi <osehiaseehilen@gmail.com>
2020-06-05 03:26:30 +0100

    signInViewModel completed

--- app/src/main/java/com/example/nescafe_pushcart/viewModel/SignInViewModel.kt
+++ app/src/main/java/com/example/nescafe_pushcart/viewModel/SignInViewModel.kt
@@ -1,4 +1,42 @@
 package com.example.nescafe_pushcart.viewModel
 
-class SignInViewModel {
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.nescafe_pushcart.model.login.LoginBody
+import com.example.nescafe_pushcart.model.login.SignInResponse
+import com.example.nescafe_pushcart.network.NetworkRepository
+import com.example.nescafe_pushcart.utils.Result
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+import retrofit2.Response
+
+class SignInViewModel: ViewModel() {
+
+    private val repository = NetworkRepository()
+    private val job = Job()
+    private val viewModelScope = CoroutineScope(job + Dispatchers.Main)
+
+
+    private val _userSignedInDetails = MutableLiveData<Result<Response<SignInResponse>>>()
+    val userSignedInDetails:LiveData<Result<Response<SignInResponse>>>
+    get() = _userSignedInDetails
+
+
+    fun signedIn(body: LoginBody):LiveData<Result<Response<SignInResponse>>>{
+
+        viewModelScope.launch {
+            _userSignedInDetails.value = repository.getSignInResponse(body)
+        }
+        return _userSignedInDetails
+    }
+
+    override fun onCleared() {
+        super.onCleared()
+        job.cancel()
+    }
+    
+
 }
