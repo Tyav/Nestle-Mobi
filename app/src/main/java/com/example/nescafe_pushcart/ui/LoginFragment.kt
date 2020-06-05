dcb94719c16be2f049f811c7c5419204c31e5b59
Osehi <osehiaseehilen@gmail.com>
2020-06-05 04:32:00 +0100

    onclickListener set on login_btn

--- app/src/main/java/com/example/nescafe_pushcart/ui/LoginFragment.kt
+++ app/src/main/java/com/example/nescafe_pushcart/ui/LoginFragment.kt
@@ -1,11 +1,14 @@
 package com.example.nescafe_pushcart.ui
 
 import android.os.Bundle
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.lifecycle.LiveData
+import androidx.lifecycle.Observer
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.fragment.findNavController
 import com.example.nescafe_pushcart.R
@@ -48,6 +51,31 @@ class LoginFragment : Fragment() {
 //        reset_password.setOnClickListener {
 //            findNavController().navigate(R.id.action_loginFragment_to_vendorProfile)
 //        }
+
+        login_btn.setOnClickListener {
+
+            val response = login()
+
+            response.observe(viewLifecycleOwner, Observer {
+
+                Log.d(TAG, "what is inside:$it")
+                when(it){
+
+                    is Result.Success -> {
+                        findNavController().navigate(R.id.action_loginFragment_to_nescafeKitchen)
+                    }
+                    is Result.Error -> {
+                        Toast.makeText(
+                            context,
+                            "${it.exception.message}",
+                            Toast.LENGTH_LONG
+                        ).show()
+                    }
+                }
+
+            })
+
+        }
 
 
 
