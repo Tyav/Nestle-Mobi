a5ace0ad13fb112d9759ce3f15f663c11d7e99aa
Osehi <osehiaseehilen@gmail.com>
2020-06-05 02:56:28 +0100

    sign_in network call at network repository written

--- app/src/main/java/com/example/nescafe_pushcart/network/NetworkRepository.kt
+++ app/src/main/java/com/example/nescafe_pushcart/network/NetworkRepository.kt
@@ -1,4 +1,37 @@
 package com.example.nescafe_pushcart.network
 
-class NetworkRepository {
+import android.util.Log
+import com.example.nescafe_pushcart.api.LoginAPI
+import com.example.nescafe_pushcart.model.login.LoginBody
+import com.example.nescafe_pushcart.model.login.SignInResponse
+import com.example.nescafe_pushcart.utils.BaseRepository
+import com.example.nescafe_pushcart.utils.Result
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.withContext
+import retrofit2.Response
+
+class NetworkRepository: BaseRepository() {
+
+    val loginAPI = LoginAPI()
+
+    val TAG = "NETWORK REPOSITORY"
+
+    suspend fun getSignInResponse(body: LoginBody): Result<Response<SignInResponse>>{
+
+        return withContext(Dispatchers.IO){
+
+            try {
+                val output = loginAPI.loginAsync(body)
+                Log.d(TAG, "show me the response: ${output}")
+                Result.Success(output)
+
+            } catch (t:Throwable){
+                Log.d(TAG, t.message.toString())
+                Result.Error(t as Exception)
+            }
+        }
+
+    }
+
+
 }
