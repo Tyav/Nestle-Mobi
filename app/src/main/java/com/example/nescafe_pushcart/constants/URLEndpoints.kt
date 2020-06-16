464844c9dee28f83ececaf9d478acb3df711bb11
Osehi <osehiaseehilen@gmail.com>
2020-06-16 21:08:06 +0100

    list of vendor endpoint

--- app/src/main/java/com/example/nescafe_pushcart/constants/URLEndpoints.kt
+++ app/src/main/java/com/example/nescafe_pushcart/constants/URLEndpoints.kt
@@ -5,6 +5,7 @@ interface URLEndpoints {
     companion object {
 
         const val AUTHENTICATE_LOGIN_ENDPOINT = "auth/login"
+        const val LIST_OF_VENDOR_ENDPOINT = "admin/vendor"
 
     }
 
