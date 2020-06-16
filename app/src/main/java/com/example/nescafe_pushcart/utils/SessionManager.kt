ce5f4142e3a27f6c8010d3d241da9e1f06373b0b
Osehi <osehiaseehilen@gmail.com>
2020-06-16 11:31:48 +0100

    session manager class completed

--- app/src/main/java/com/example/nescafe_pushcart/utils/SessionManager.kt
+++ app/src/main/java/com/example/nescafe_pushcart/utils/SessionManager.kt
@@ -1,4 +1,38 @@
 package com.example.nescafe_pushcart.utils
 
-class SessionManager {
+import android.content.Context
+import android.content.SharedPreferences
+
+
+/**
+ * Session manager to save and fetch data from SharedPreferences
+ */
+
+class SessionManager(context: Context) {
+
+    private var prefs:SharedPreferences = context.getSharedPreferences("Retrofit Authorization", Context.MODE_PRIVATE)
+
+    companion object {
+        const val USER_TOKEN = "user_token"
+    }
+
+    /**
+     * Function to save auth token
+     */
+
+    fun saveAuthToken(token:String){
+        val editor = prefs.edit()
+        editor.putString(USER_TOKEN, "user token")
+        editor.apply()
+
+    }
+
+    /**
+     * Function to fetch auth token
+     */
+
+    fun fetchAuthToken():String?{
+        return prefs.getString(USER_TOKEN, null)
+    }
+
 }
