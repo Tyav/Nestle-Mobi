3a16cffc06d707538274276ac84b84e6523136a3
Osehi <osehiaseehilen@gmail.com>
2020-06-17 16:45:57 +0100

    NescafeKitchenViewModel completed

--- app/src/main/java/com/example/nescafe_pushcart/viewModel/NescafeKitchenViewModel.kt
+++ app/src/main/java/com/example/nescafe_pushcart/viewModel/NescafeKitchenViewModel.kt
@@ -1,4 +1,41 @@
 package com.example.nescafe_pushcart.viewModel
 
-class NescafeKitchenViewModel {
+import android.app.Application
+import androidx.lifecycle.AndroidViewModel
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+import com.example.nescafe_pushcart.model.listofvendors.VendorList
+import com.example.nescafe_pushcart.network.NetworkRepository
+import com.example.nescafe_pushcart.utils.Result
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.Job
+import kotlinx.coroutines.launch
+
+class NescafeKitchenViewModel(application: Application): AndroidViewModel(application) {
+
+    private val repository = NetworkRepository(getApplication())
+    private val job = Job()
+    private val viewModelScope = CoroutineScope(job + Dispatchers.Main)
+
+    private val _myVendorList = MutableLiveData<Result<VendorList>>()
+    val myVendorList:LiveData<Result<VendorList>>
+    get() = _myVendorList
+
+
+
+    fun getVendorListNow():LiveData<Result<VendorList>>{
+
+        viewModelScope.launch {
+
+            _myVendorList.value = repository.getVendorList()
+
+        }
+
+        return _myVendorList
+
+    }
+
+
 }
