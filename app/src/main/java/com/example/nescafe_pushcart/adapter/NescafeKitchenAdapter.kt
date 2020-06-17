42b123a28b2d964bcce080740a4586ded53eb773
Osehi <osehiaseehilen@gmail.com>
2020-06-17 18:02:39 +0100

    NescafeKitchen adapter completed

--- app/src/main/java/com/example/nescafe_pushcart/adapter/NescafeKitchenAdapter.kt
+++ app/src/main/java/com/example/nescafe_pushcart/adapter/NescafeKitchenAdapter.kt
@@ -1,4 +1,52 @@
 package com.example.nescafe_pushcart.adapter
 
-class NescafeKitchenAdapter {
+import android.view.LayoutInflater
+import android.view.ViewGroup
+import androidx.recyclerview.widget.DiffUtil
+import androidx.recyclerview.widget.ListAdapter
+import androidx.recyclerview.widget.RecyclerView
+import com.example.nescafe_pushcart.databinding.FragmentNescafeKitchenBinding
+import com.example.nescafe_pushcart.databinding.ListItemVendorinkitchenBinding
+import com.example.nescafe_pushcart.model.listofvendors.VendorList
+
+class NescafeKitchenAdapter(val onClickListener:OnClickListener):ListAdapter<VendorList, NescafeKitchenAdapter.NescafeKitchenViewHolder>(DiffCallback) {
+
+
+    class NescafeKitchenViewHolder(var binding: ListItemVendorinkitchenBinding):RecyclerView.ViewHolder(binding.root){
+
+        fun bind(vendorList: VendorList){
+            binding.vendorList = vendorList
+
+            binding.executePendingBindings()
+        }
+
+    }
+
+    companion object DiffCallback:DiffUtil.ItemCallback<VendorList>(){
+        override fun areItemsTheSame(oldItem: VendorList, newItem: VendorList): Boolean {
+            return oldItem == newItem
+        }
+
+        override fun areContentsTheSame(oldItem: VendorList, newItem: VendorList): Boolean {
+            return oldItem.data == oldItem.data
+        }
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): NescafeKitchenViewHolder {
+        return NescafeKitchenViewHolder(ListItemVendorinkitchenBinding.inflate(LayoutInflater.from(parent.context)))
+    }
+
+    override fun onBindViewHolder(holder: NescafeKitchenViewHolder, position: Int) {
+        val vendorList = getItem(position)
+
+        holder.bind(vendorList)
+
+    }
+
+    class OnClickListener(val clickListener:(vendorList:VendorList) -> Unit){
+
+        fun onClick(vendorList:VendorList) = clickListener(vendorList)
+
+    }
+
 }
--- app/src/main/java/com/example/nescafe_pushcart/ui/NescafeKitchen.kt
+++ app/src/main/java/com/example/nescafe_pushcart/ui/NescafeKitchen.kt
@@ -7,18 +7,26 @@ import android.view.View
 import android.view.ViewGroup
 
 import com.example.nescafe_pushcart.R
+import com.example.nescafe_pushcart.databinding.FragmentNescafeKitchenBinding
 
 /**
  * A simple [Fragment] subclass.
  */
 class NescafeKitchen : Fragment() {
 
+    private lateinit var binding: FragmentNescafeKitchenBinding
+
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
         // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_nescafe_kitchen, container, false)
+//        return inflater.inflate(R.layout.fragment_nescafe_kitchen, container, false)
+
+        binding = FragmentNescafeKitchenBinding.inflate(inflater, container, false)
+
+        return binding.root
+
     }
 
 }
